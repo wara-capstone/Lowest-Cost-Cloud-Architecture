4c8a2e15547480311f63bead8a20535591facf01
mimijae <qzqzcarazOO@gmail.com>
2023-10-06 04:42:49 +0900

    readme 개선 및 프론트 수정

--- README.md
+++ README.md
@@ -1,4 +1,18 @@
-﻿# thesis_MJMK
+﻿# **저예산 고성능 클라우드 아키텍처: 스타트업을 위한 최적화 전략**
+## [IaaS 3-tier architecture와 PaaS 성능 비교]
+## **1. 주요 개요**
+- **비용 효율성**: 클라우드 환경에서의 고성능과 저비용을 동시에 추구하는 것이 필요하다.
+- **분산 파일 시스템**: 높은 성능을 제공하면서 비용을 절감할 수 있는 기술 적용이 중요하다.
+- **스타트업의 필요성**: 클라우드 환경에서 스타트업이 필요로 하는 기능의 설계와 구축에 중점을 둔다.
+
+## **2. 테스트 방법**
+- **3계층구조 와 PaaS서버 부하**: 다양한 시나리오에서의 서버 부하를 테스트한다.
+- **성능 비교**: 다양한 지표(디비성능, 서버성능, 관리 및 유지 비용 등)를 기반으로 성능을 비교 분석한다.
+
+## **3. 클라우드 서비스 이해**
+- **장단점**: 스타트업에서 최적의 성능을 위해 클라우드 서비스의 특징과 장단점을 깊게 파악한다.
+
+
 
 ## web Ubuntu 서버 베포
 
@@ -58,6 +72,19 @@ sudo docker-compose up -d --build
 
 ```bash
 sudo docker-compose down
+```
+
+**프론트서버 gunicorn을 사용해 정적파일 관리**
+```bash
+sudo docker-compose exec web python manage.py collectstatic
+```
+
+```bash
+sudo docker-compose exec web python manage.py collectstatic --noinput
+```
+
+```bash
+sudo docker-compose restart nginx
 ```
 
 
--- web/script.js
+++ web/script.js
@@ -1,8 +1,4 @@
-const BASE_URL = 'http://192.17.15.1';
-
-document.getElementById('sendData').addEventListener('click', function() {
-    performRequest('sendData');
-});
+const BASE_URL = 'http://223.130.137.75';
 
 document.getElementById('generateTraffic1000').addEventListener('click', function() {
     performRequest('generateTraffic1000');
@@ -11,60 +7,175 @@ document.getElementById('generateTraffic1000').addEventListener('click', functio
 document.getElementById('generateTraffic10000').addEventListener('click', function() {
     performRequest('generateTraffic10000');
 });
+document.getElementById('prevPage').addEventListener('click', function() {
+    changePage(-1);
+});
+document.getElementById('nextPage').addEventListener('click', function() {
+    changePage(1);
+});
+document.getElementById('sendData').addEventListener('click', getFruitInstance);
 
-function performRequest(buttonId) {
+document.getElementById('showAll').addEventListener('click', function() {
+    if (isShowingAll) {
+        isShowingAll = false;
+        renderTable();
+    } else {
+        showAllRows();
+    }
+});
+
+let isShowingAll = false;
+
+function getFruitInstance() {
+    const startTime = new Date();
     const selectedFruit = document.getElementById('fruitSelection').value;
+    const loadingTimeSendData = document.getElementById('loadingTimeSendData');
 
+    fetch(`${BASE_URL}/fruits/?name=${selectedFruit}`, {
+        method: 'GET',
+        mode: 'cors',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        const endTime = new Date();
+        const elapsedTime = (endTime - startTime) / 1000;
+        loadingTimeSendData.textContent += ` ${elapsedTime.toFixed(2)} 초`;
+
+        const tbody = document.getElementById('responseBody');
+        while(tbody.firstChild) {
+            tbody.removeChild(tbody.firstChild);
+        }
+
+        data.forEach((fruit, index) => {
+            const newRow = tbody.insertRow();
+            const numberCell = newRow.insertCell(0);
+            numberCell.className = 'numberCell';
+            const fruitCell = newRow.insertCell(1);
+            const responseCell = newRow.insertCell(2);
+
+            numberCell.textContent = index + 1;
+            fruitCell.textContent = fruit.name;
+            responseCell.textContent = fruit.message;
+        });
+
+        currentPage = 1;  // 페이지 초기화
+        renderTable();    // 테이블 재렌더링
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        document.getElementById('response').innerText = 'Error occurred while fetching fruit instance data.';
+    });
+}
+
+async function performRequest(buttonId) {
+    const selectedFruit = document.getElementById('fruitSelection').value;
     const loadingDiv = document.getElementById('loading');
-    const loadingTimeDiv = {
+    const loadingTimeDiv = document.getElementById('loadingTime');
+    const recordTimeDiv = {
         sendData: document.getElementById('loadingTimeSendData'),
         generateTraffic1000: document.getElementById('loadingTime1000'),
         generateTraffic10000: document.getElementById('loadingTime10000')
     }[buttonId];
 
-    const startTime = new Date(); // 요청 시작 시간
+    const startTime = new Date();
+    
+    let secondsPassed = 0.00;
+    loadingDiv.style.display = 'flex';
 
-    loadingDiv.style.display = 'flex'; // 로딩 창 표시
+    const interval = setInterval(() => {
+        secondsPassed += 0.01; // Increase by 0.1 every 100ms
+        loadingTimeDiv.textContent = `${secondsPassed.toFixed(2)} seconds`;
+    }, 10); // Run every 100ms
 
     let fetchCount = 1;
-    if(buttonId === 'generateTraffic1000') fetchCount = 1000;
-    if(buttonId === 'generateTraffic10000') fetchCount = 10000;
+    if (buttonId === 'generateTraffic1000') fetchCount = 1000;
+    if (buttonId === 'generateTraffic10000') fetchCount = 10000;
 
-    for(let i = 0; i < fetchCount; i++) {
-        fetch(`${BASE_URL}/api/data`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ fruit: selectedFruit })
-        })
-        .then(response => response.json())
-        .then(data => {
-            const endTime = new Date(); // 요청 완료 시간
-            const elapsedTime = (endTime - startTime) / 1000; // 초 단위로 경과 시간 계산
+    const requestBody = (buttonId === 'sendData') ? { fruit: selectedFruit } : { fruit: 'ready' };
+    const CHUNK_SIZE = 50;
+    let completedRequests = 0;
 
-            loadingTimeDiv.innerText = `Loaded in ${elapsedTime.toFixed(2)} seconds`; // 로딩 시간 표시
-
-            if(buttonId === 'sendData') {
-                const tbody = document.getElementById('responseBody');
-                const newRow = tbody.insertRow();
-                const fruitCell = newRow.insertCell(0);
-                const responseCell = newRow.insertCell(1);
-                fruitCell.textContent = selectedFruit;
-                responseCell.textContent = data.message;
+    try {
+        for (let i = 0; i < fetchCount; i += CHUNK_SIZE) {
+            const fetchPromises = [];
+            for (let j = i; j < i + CHUNK_SIZE && j < fetchCount; j++) {
+                fetchPromises.push(
+                    fetch(`${BASE_URL}/fruits/`, {
+                        method: 'POST',
+                        mode: 'cors',
+                        headers: {
+                            'Content-Type': 'application/json'
+                        },
+                        body: JSON.stringify(requestBody)
+                    })
+                    .then(response => response.json())
+                    .catch(error => {
+                        console.error('Error:', error);
+                    })
+                );
             }
-        })
-        .catch(error => {
-            console.error('Error:', error);
-            document.getElementById('response').innerText = 'Error occurred while fetching data.';
-            loadingDiv.style.display = 'none'; // 에러 발생 시 로딩 창 숨기기
-        })
-        .finally(() => {
-            if (i === fetchCount - 1) { // 마지막 요청이 완료되면 로딩 창을 숨깁니다.
-                setTimeout(() => {
-                    loadingDiv.style.display = 'none';
-                }, 2000); // 2초 후에 로딩 창을 숨깁니다.
-            }
-        });
+
+            await Promise.all(fetchPromises);
+            completedRequests += fetchPromises.length;
+            console.log(`${completedRequests} requests completed.`);
+        }
+    } finally {
+        clearInterval(interval);
+
+        const endTime = new Date();
+        const elapsedTime = (endTime - startTime) / 1000;
+        recordTimeDiv.textContent += ` ${elapsedTime.toFixed(2)} seconds`;
+        
+        loadingDiv.style.display = 'none';
     }
 }
+
+function showAllRows() {
+    const table = document.getElementById("responseBody");
+    const rows = table.querySelectorAll("tr");
+
+    // 모든 행을 표시
+    rows.forEach(row => {
+        row.style.display = '';
+    });
+
+    isShowingAll = true; // 전체 보기 모드 활성화
+}
+
+function changePage(direction) {
+    currentPage += direction;
+    renderTable();
+}
+
+let currentPage = 1;
+const rowsPerPage = 20;
+
+function renderTable() {
+    if (isShowingAll) {
+        isShowingAll = false;
+    }
+
+    const table = document.getElementById("responseBody");
+    const rows = table.querySelectorAll("tr");
+    const totalRows = rows.length;
+    const totalPages = Math.ceil(totalRows / rowsPerPage);
+
+    document.getElementById("pageIndicator").textContent = `${currentPage} / ${totalPages}`;
+
+    rows.forEach(row => {
+        row.style.display = 'none';
+    });
+
+    for(let i = (currentPage - 1) * rowsPerPage; i < currentPage * rowsPerPage && i < totalRows; i++) {
+        rows[i].style.display = '';
+    }
+
+    document.getElementById("prevPage").disabled = currentPage === 1;
+    document.getElementById("nextPage").disabled = currentPage * rowsPerPage >= totalRows;
+}
+
+// 초기 테이블 렌더링
+renderTable();
