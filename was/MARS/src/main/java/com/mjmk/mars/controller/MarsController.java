3d6d94be2b04474e4b4a34e731d4c4c67d1a1adc
kormk <63334787+kormk@users.noreply.github.com>
2023-10-06 22:43:34 +0900

    :white_check_mark: test-code 작성/배포 완료

--- was/MARS/src/main/java/com/mjmk/mars/controller/MarsController.java
+++ was/MARS/src/main/java/com/mjmk/mars/controller/MarsController.java
@@ -5,11 +5,14 @@ package com.mjmk.mars.controller;
 import com.mjmk.mars.dto.MarsDTO;
 import com.mjmk.mars.service.MarsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
 
 import java.util.List;
 
 @RestController
+//@CrossOrigin(origins = "http://118.67.130.119")
 public class MarsController {
 
     private final MarsService service;
@@ -21,8 +24,8 @@ public class MarsController {
     /**
      * @param name 해당 과일 데이터를 모두 반환
      * */
-    @GetMapping(path = "fruit/{name}")
-    public List<MarsDTO> dummyGet(@PathVariable String name)
+    @GetMapping(path = "fruits/{name}")
+    public List<MarsDTO> dummyGet(@RequestBody @PathVariable String name)
     {
         return service.testRead(name);
     }
@@ -32,9 +35,12 @@ public class MarsController {
      * "ready"를 받고 "OK"를 리턴
      *
      * */
-    @PostMapping(path = "fruits/{name}")
-    public String dummyPost(@PathVariable String name) {
-        return service.testCreat(name);
+
+    @PostMapping(path = "fruits")
+    public MarsDTO dummyPost(@RequestBody String name) {
+        service.testCreat(name);
+
+        return new MarsDTO("OK", 1L);
     }
 
 
